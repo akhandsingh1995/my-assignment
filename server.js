ea32fec0c8dc96eecb7be9505fa4f4e9278b4609
akhandsingh1995 <61067437+akhandsingh1995@users.noreply.github.com>
2020-02-25 22:31:24 +0530

    Delete server.js

--- server.js
+++
@@ -1,8 +0,0 @@
-var express = require('express');
-var app = express();
-var http = require('http')
-
-require('./app.js')(app);
-http.createServer(app).listen(4000, () => {
-    console.log('Server Created')
-})
